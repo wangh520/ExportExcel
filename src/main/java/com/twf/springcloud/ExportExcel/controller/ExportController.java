3fab17955530b271a47a3f7c65417d8a00680d8e
wangh520 <52517380+a807920472@users.noreply.github.com>
2020-08-13 10:34:46 +0800

    修改

--- src/main/java/com/twf/springcloud/ExportExcel/controller/ExportController.java
+++ src/main/java/com/twf/springcloud/ExportExcel/controller/ExportController.java
@@ -7,13 +7,14 @@ import com.twf.springcloud.ExportExcel.utils.ExcelPOIUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,7 +38,10 @@ public class ExportController {
 	// 导入
 	@RequestMapping(value = "importData")
 	@ResponseBody
-	public BaseDataResult importData(@RequestParam MultipartFile file) {
+	public BaseDataResult importData(HttpServletRequest request) {
+
+		MultipartHttpServletRequest req = (MultipartHttpServletRequest) request;
+		MultipartFile file = req.getFile("1.xlsx");
 		try {
 			//错误maps
 			Map<Integer, String> errorMap = new HashMap<>();
@@ -48,9 +52,14 @@ public class ExportController {
 			HashMap<String, Object> maps = new HashMap<>();
 			maps.put("succ", stations);
 			maps.put("fail", errorMap);
-			return new BaseDataResult("400", "导入成功");
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			out.write(maps.toString().getBytes());
+			out.flush();
+
+			return new BaseDataResult("400", "导入1成功1");
 		} catch (Exception e) {
-			return new BaseDataResult("", e.getMessage());
+			return new BaseDataResult("444444444", e.getMessage());
 		}
 	}
 }
